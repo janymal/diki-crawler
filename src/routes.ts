9e73f26dd90bddbacd49d7ea2d725cb1225ef2d4
Jan Kot <janymalino@gmail.com>
2024-02-01 18:49:07 +0100

    prettier format

--- src/routes.ts
+++ src/routes.ts
@@ -37,63 +37,106 @@ interface IExampleSentences {
 }
 
 router.addHandler("detail", async ({ $, pushData, request, log }) => {
-  const dictionaryEntities = $("div .diki-results-left-column").find("div .dictionaryEntity");
+  const dictionaryEntities = $("div .diki-results-left-column").find(
+    "div .dictionaryEntity",
+  );
   dictionaryEntities.each((_, el) => {
     const entity: IEntity = {
       hws: [],
-      meaningGroups: []
+      meaningGroups: [],
     };
-    $(el).find("h1").each((_, el) => {
-      $(el).children(".hw").each((_, el) => {
-        const recordingAndTranscriptions = $(el).nextAll(".recordingsAndTranscriptions").first();
-        const additionalInformations = $(el).nextAll(".dictionaryEntryHeaderAdditionalInformation").first();
-        let popularity = 0;
-        let variety = undefined;
-        additionalInformations.children().each((_, el) => {
-          if ($(el).hasClass("starsForNumOccurrences"))
-            popularity = $(el).text().length;
-          if ($(el).hasClass("languageVariety"))
-            variety = $(el).text();
-        });
-        const recordings = recordingAndTranscriptions?.find(".hasRecording").map((_, el) => {
-          const r: IRecording = {
-            lang: $(el).attr("class")?.split(" ")[0]!,
-            url: url.resolve(request.url, $(el).find(".soundOnClick").first().attr("data-audio-url")!),
-          };
-          return r;
-        }).toArray();
-        const hw: IHw = {
-          title: $(el).text().trim(),
-          transcription: recordingAndTranscriptions?.find(".phoneticTranscription").first().find("img").first().attr(
-            "src",
-          ),
-          recordings: recordings ? recordings : [],
-          popularity: popularity,
-          variety: variety
+    $(el)
+      .find("h1")
+      .each((_, el) => {
+        $(el)
+          .children(".hw")
+          .each((_, el) => {
+            const recordingAndTranscriptions = $(el)
+              .nextAll(".recordingsAndTranscriptions")
+              .first();
+            const additionalInformations = $(el)
+              .nextAll(".dictionaryEntryHeaderAdditionalInformation")
+              .first();
+            let popularity = 0;
+            let variety = undefined;
+            additionalInformations.children().each((_, el) => {
+              if ($(el).hasClass("starsForNumOccurrences"))
+                popularity = $(el).text().length;
+              if ($(el).hasClass("languageVariety")) variety = $(el).text();
+            });
+            const recordings = recordingAndTranscriptions
+              ?.find(".hasRecording")
+              .map((_, el) => {
+                const r: IRecording = {
+                  lang: $(el).attr("class")?.split(" ")[0]!,
+                  url: url.resolve(
+                    request.url,
+                    $(el).find(".soundOnClick").first().attr("data-audio-url")!,
+                  ),
+                };
+                return r;
+              })
+              .toArray();
+            const hw: IHw = {
+              title: $(el).text().trim(),
+              transcription: recordingAndTranscriptions
+                ?.find(".phoneticTranscription")
+                .first()
+                .find("img")
+                .first()
+                .attr("src"),
+              recordings: recordings ? recordings : [],
+              popularity: popularity,
+              variety: variety,
+            };
+            entity.hws.push(hw);
+          });
+      });
+    $(el)
+      .children(".partOfSpeechSectionHeader")
+      .each((_, el) => {
+        const meaningGroup: IMeaningGroup = {
+          partOfSpeech: $(el).find(".partOfSpeech").first().text(),
+          meanings: [],
         };
-        entity.hws.push(hw);
+        $(el)
+          .nextAll(".foreignToNativeMeanings")
+          .first()
+          .children("li")
+          .each((_, el) => {
+            const meaning: IMeaning = {
+              hws: $(el)
+                .find(".hw")
+                .map((_, el) => $(el).text().trim())
+                .toArray(),
+              exampleSentences: $(el)
+                .find(".exampleSentence")
+                .map((_, el) => {
+                  const translation = $(el)
+                    .find(".exampleSentenceTranslation")
+                    .first()
+                    .text()
+                    .trim();
+                  return {
+                    sentence: $(el)
+                      .contents()
+                      .filter(function () {
+                        return this.nodeType == 3;
+                      })
+                      .text()
+                      .trim(),
+                    translation: translation.substring(
+                      1,
+                      translation.length - 1,
+                    ),
+                  };
+                })
+                .toArray(),
+            };
+            meaningGroup.meanings.push(meaning);
+          });
+        entity.meaningGroups.push(meaningGroup);
       });
-    });
-    $(el).children(".partOfSpeechSectionHeader").each((_, el) => {
-      const meaningGroup: IMeaningGroup = {
-        partOfSpeech: $(el).find(".partOfSpeech").first().text(),
-        meanings: []
-      }
-      $(el).nextAll(".foreignToNativeMeanings").first().children("li").each((_, el) => {
-        const meaning: IMeaning = {
-          hws: $(el).find(".hw").map((_, el) => $(el).text().trim()).toArray(),
-          exampleSentences: $(el).find(".exampleSentence").map((_, el) => {
-            const translation = $(el).find(".exampleSentenceTranslation").first().text().trim();
-            return {
-              sentence: $(el).contents().filter(function() { return this.nodeType == 3;}).text().trim(),
-              translation: translation.substring(1, translation.length - 1)
-            }
-          }).toArray()
-        }
-        meaningGroup.meanings.push(meaning);
-      });
-      entity.meaningGroups.push(meaningGroup);
-    });
     pushData({ entity });
   });
 });
