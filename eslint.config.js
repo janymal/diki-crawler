181613fa38e47581cb399986127f14b83d87426e
Jan Kot <janymalino@gmail.com>
2024-02-09 13:18:58 +0100

    setup the eslint

---
+++ eslint.config.js
@@ -0,0 +1,27 @@
+import js from "@eslint/js";
+import typescriptPlugin from "@typescript-eslint/eslint-plugin";
+import typescriptParser from "@typescript-eslint/parser";
+import unicorn from "eslint-plugin-unicorn";
+
+export default [
+  js.configs.recommended,
+  typescriptPlugin.configs["eslint-recommended"].overrides[0],
+  unicorn.configs["flat/recommended"],
+  {
+    languageOptions: {
+      parser: typescriptParser,
+    },
+    plugins: {
+      "@typescript-eslint": typescriptPlugin,
+    },
+    rules: typescriptPlugin.configs.strict.rules,
+  },
+  {
+    files: ["src/**.ts"],
+    rules: {
+      "unicorn/prevent-abbreviations": 0,
+      "sort-imports": 1,
+      "eqeqeq": 2,
+    },
+  },
+];
