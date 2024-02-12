c29daeae3e44e6e84f5f53e1058a14acd2bb3273
Jan Kot <janymalino@gmail.com>
2024-02-12 23:07:18 +0100

    Revert "add partOfSpeech field to the Header class"
    This reverts commit d26b4e55cc6910ffadd3b5012d8ff8765604c3b5.

--- src/routes.ts
+++ src/routes.ts
@@ -364,7 +364,6 @@ class Header
     readonly lessPopular: boolean,
     readonly additionalInformation?: AdditionalInformation,
     readonly recordingsAndTranscriptions?: RecordingsAndTranscriptions,
-    readonly partOfSpeech?: string,
   )
   {}
 
@@ -393,9 +392,7 @@ class Header
           context,
           child,
         );
-      } else if (child.hasClass("headerPartOfSpeech"))
-        data.partOfSpeech = child.text();
-      else if (child.hasClass("hwcomma") || child.prop("tagName") === "BR")
+      } else if (child.hasClass("hwcomma") || child.prop("tagName") === "BR")
         return;
       else
         logUnknownItem(context, child, this.name);
